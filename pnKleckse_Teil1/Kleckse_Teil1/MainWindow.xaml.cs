87678993799baded2c45885a42046a986e7836ac
rs sr <rsdaten@live.de>
2016-10-22 01:32:02 +0200

    ***Kleckse_Teil2 ---Klecksen bewegen sich

--- pnKleckse_Teil1/Kleckse_Teil1/MainWindow.xaml.cs
+++ pnKleckse_Teil1/Kleckse_Teil1/MainWindow.xaml.cs
@@ -23,10 +23,15 @@ namespace Kleckse_Teil1
         const int numPoints = 5; //Klecks Anzahl
         double[,] xy = new double[numPoints, 2]; // mehrdimensionale Array fuer Kleks Koordinaten
         double[][] distanz = new double[numPoints][]; // jagged Array fuer Abstaende zwischen Kleks
-        Random rnd = new Random();
+        Ellipse[] ellipses = new Ellipse[numPoints]; // eindimensionale Array von Typ "Ellipse"
+
+        Random random = new Random();
+
+        System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer(); // Timer erzeugen
 
 
-        
+
+
 
         public MainWindow()
         {
@@ -41,13 +46,45 @@ namespace Kleckse_Teil1
             {
                 for (int j = 0; j < distanz[i].Length; j++)
                 {
-                    distanz[i][j] = rnd.NextDouble() * 100.0; //jagged Array mit Random Daten fuellen
+                    distanz[i][j] = random.NextDouble() * 100.0; //jagged Array mit Random Daten fuellen
                 }
             }
+
+            for (int i = 0; i < numPoints; i++) // Kleckse erstellen
+            {
+                Ellipse myEllipse = new Ellipse();
+                myEllipse.Fill = Brushes.Blue; //Farbe fuer Kleckse
+                myEllipse.Width = 10.0;
+                myEllipse.Height = 10.0;
+                myCanvas.Children.Add(myEllipse); // Kleckse Ausgabe
+
+                ellipses[i] = myEllipse;
+            }
+
+
+            timer.Interval = TimeSpan.FromSeconds(0.1); // Timer interval
+            timer.Tick += layout; // Methode die aufgerufen werden soll !!! += !!! nutzen
+            timer.IsEnabled = true; // Timer einschalten
 
 
 
             string testStopPoint = "###STOP###STOP###STOP###";
+        }
+
+        void layout(object sender, EventArgs e)
+        {
+            Ellipse myEllipse = new Ellipse();
+            for (int i = 0; i < numPoints; i++)
+            {
+                xy[i, 0] += 5 * random.NextDouble(); // neue Random x- Koordinate
+                xy[i, 1] += 5 * random.NextDouble(); // neue Random y- Koordinate
+            }
+
+            for (int i = 0; i < numPoints; i++) // Kleckse erstellen
+            {
+                Canvas.SetLeft(ellipses[i], xy[i, 0]); // Klecks an "Canvas" positionieren
+                Canvas.SetTop(ellipses[i], xy[i, 1]); // Klecks an "Canvas" positionieren
+            }
         }
     }
 }
