d1f944131e3db0d911f552efa56bea680fa7d90d
rs sr <rsdaten@live.de>
2016-10-23 01:42:31 +0200

    ***Start ---Klass Meeting erstellt

---
+++ pnMeeting/Meeting/Meeting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meeting
+{
+    class Meeting
+    {
+        //Felder, Instanzvariablen, Attribute
+        DateTime when; //wann
+        TimeSpan duration; //dauer
+        string title;
+
+        //Methode
+        public bool IsNow() // ist Aktuell (jetzt)
+                            //wenn "public" -> IsNow.
+                            //wenn "privat" -> isNow.
+        {
+            DateTime now = DateTime.Now;
+            return now >= when && now <= when + duration;
+        }
+    }
+}
