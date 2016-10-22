e313dc135f5439d43340ac31aa5d5bebebb0f83a
rs sr <rsdaten@live.de>
2016-10-23 00:12:40 +0200

    --- Getrennt

---
+++ pnSprachausgabe/Sprachausgabe/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Speech.Synthesis;
+
+namespace Sprachausgabe
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            SpeechSynthesizer sp = new SpeechSynthesizer();
+            DateTime dt = DateTime.Now;
+
+            sp.SpeakAsync(textBox.Text);    //Text aus texBox Akustisch ausgeben            
+            sp.SpeakAsync(dt.ToLongTimeString()); //Aktuelle Zeit Akustisch ausgeben 
+
+        }
+    }
+}
---
+++ pnSpracherkennung/Spracherkennung/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+
+namespace Spracherkennung
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Choices theChoices = new Choices();
+            theChoices.Add("Bielefeld");
+            theChoices.Add("Gütersloh");
+            theChoices.Add("Minden");
+            theChoices.Add("Neuss");
+            theChoices.Add("Düsseldorf");
+            theChoices.Add("Köln");
+            theChoices.Add("Christian");
+            theChoices.Add("Irina");
+            SpeechRecognizer recognizer = new SpeechRecognizer();
+            recognizer.LoadGrammar(new Grammar(new GrammarBuilder(theChoices)));
+            recognizer.SpeechRecognized += speechRecognized;
+            recognizer.Enabled = true;
+        }
+
+        void speechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            label.Content = e.Result.Text;
+
+        }
+    }
+}
