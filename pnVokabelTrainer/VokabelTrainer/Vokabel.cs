98961da9a5628e688845fcefda530ade7b1b75a7
rs sr <rsdaten@live.de>
2016-10-25 22:03:12 +0200

    ***Start ---Class Vokabel erstellt

---
+++ pnVokabelTrainer/VokabelTrainer/Vokabel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VokabelTrainer
+{
+    class Vokabel
+    {
+        string deutchesWort;
+        string englischeUebersetzung;
+        DateTime letzteAbfrage;
+        int zahlKorrekterAbfragen;
+        int zahlFehlgeschlagenerAbfragen;
+
+        Vokabel(string deutchesWort, string englischeUebersetzung, int zahlKorrekterAbfragen, int zahlFehlgeschlagenerAbfragen)
+        {
+            this.deutchesWort = deutchesWort;
+            this.englischeUebersetzung = englischeUebersetzung; 
+        }
+
+    }
+}
