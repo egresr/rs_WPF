0e892be8b5b3e6a9e0c1824446a97b93b2e9968d
rs sr <rsdaten@live.de>
2016-10-27 07:51:24 +0200

    ---Info Hinzugefuegt am eingebaut ---Statistik Methoden ZahlKorrekteAbfragen eingebaut ---Statistik Methoden ZahlFehlgeschlageneAbfragen eingebaut

--- pnVokabelTrainer/VokabelTrainer/MainWindow.xaml.cs
+++ pnVokabelTrainer/VokabelTrainer/MainWindow.xaml.cs
@@ -20,9 +20,43 @@ namespace VokabelTrainer
     /// </summary>
     public partial class MainWindow : Window
     {
+  
+        Vokabel aktuelleVokabel;
+        Random random = new Random();
+        Vokabel[] vokabular = new Vokabel[3];
+
         public MainWindow()
-        {
+        {        
             InitializeComponent();
+
+            vokabular[0] = new Vokabel("hallo", "hello");
+            vokabular[1] = new Vokabel("gehen", "to go");
+            vokabular[2] = new Vokabel("gut", "good");
+
+            aktuelleVokabel = vokabular[random.Next(vokabular.Length)];
+            lblDatumHinzugefuegt.Content = aktuelleVokabel.ErstellungsDatum.ToShortDateString();
+
+            lblAbfrage.Content = aktuelleVokabel.DeutschesWort;    
+            lblZaehlerRichtig.Content = aktuelleVokabel.ZahlKorrekteAbfragen;
+            lblZaehlerFalsch.Content = aktuelleVokabel.ZahlFehlgeschlageneAbfragen;      
+        }
+
+        private void btnTest_Click(object sender, RoutedEventArgs e)
+        {
+            if (aktuelleVokabel.Pruefe(txtBxAngabe.Text))
+            {
+                aktuelleVokabel = vokabular[random.Next(vokabular.Length)];       
+                lblAbfrage.Content = aktuelleVokabel.DeutschesWort;
+                txtBxAngabe.Text = "";
+            }      
+            else
+            {
+                MessageBox.Show("Leider falsch!");
+            }
+            lblDatumLetzteAbfrage.Content = aktuelleVokabel.LetzteAbfrage;
+            lblZaehlerRichtig.Content = aktuelleVokabel.ZahlKorrekteAbfragen;
+            lblZaehlerFalsch.Content = aktuelleVokabel.ZahlFehlgeschlageneAbfragen;
+
         }
     }
 }
--- pnVokabelTrainer/VokabelTrainer/Vokabel.cs
+++ pnVokabelTrainer/VokabelTrainer/Vokabel.cs
@@ -10,15 +10,58 @@ namespace VokabelTrainer
     {
         string deutchesWort;
         string englischeUebersetzung;
-        DateTime letzteAbfrage;
-        int zahlKorrekterAbfragen;
-        int zahlFehlgeschlagenerAbfragen;
+        DateTime erstellungsDatum;
+        DateTime letzteAbfrage;   //TODO
+        int zahlKorrekterAbfragen;  //TODO
+        int zahlFehlgeschlagenerAbfragen; //TODO
 
-        Vokabel(string deutchesWort, string englischeUebersetzung, int zahlKorrekterAbfragen, int zahlFehlgeschlagenerAbfragen)
+        public string DeutschesWort
         {
-            this.deutchesWort = deutchesWort;
-            this.englischeUebersetzung = englischeUebersetzung; 
+            get { return deutchesWort; }
+            set { }
+        } 
+
+        public int ZahlKorrekteAbfragen
+        {
+            get { return zahlKorrekterAbfragen; }
         }
 
+        public int ZahlFehlgeschlageneAbfragen
+        {
+            get { return zahlFehlgeschlagenerAbfragen; }
+        }
+
+        public DateTime LetzteAbfrage
+        {
+            get { return letzteAbfrage; }
+        }
+
+        public DateTime ErstellungsDatum
+        {
+            get { return erstellungsDatum; }
+        }
+
+        public Vokabel(string deutchesWort, string englischeUebersetzung)
+        {
+            this.deutchesWort = deutchesWort;
+            this.englischeUebersetzung = englischeUebersetzung;
+            erstellungsDatum = DateTime.Now;
+        }
+
+        public bool Pruefe(string text)
+        {
+            letzteAbfrage = DateTime.Now;
+
+            if (englischeUebersetzung == text)
+            {
+                zahlKorrekterAbfragen ++;
+                return true;
+            }
+            else
+            {
+                zahlFehlgeschlagenerAbfragen ++;
+                return false;
+            }  
+        }
     }
 }
